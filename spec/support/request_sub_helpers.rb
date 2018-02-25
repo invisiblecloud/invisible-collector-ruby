3758f594e5e6080bcc50ee766f4cec275b2e71af
João Madureira <jpmadureira@gmail.com>
2018-02-25 22:34:17 +0000

    Fixed spec tests.

--- spec/support/request_sub_helpers.rb
+++ spec/support/request_sub_helpers.rb
@@ -1,7 +1,7 @@
 module RequestStubHelpers
 
   def stub_do_api(path, verb = :any)
-    stub_request(verb, %r[#{InvoiceCapture::API::INVOICECAPTURE_API}#{Regexp.escape(path)}])
+    stub_request(verb, %r[#{InvoiceCapture::API::INVISIBLECOLLECTOR_API}#{Regexp.escape(path)}])
   end
 
   def api_fixture(fixture_name)
